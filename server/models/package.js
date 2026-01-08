f555b4c73409d7a28f7e1cd18bc38d8cf2be3403
unknown <ze.mohramy@gmail.com>
2026-01-08 16:33:57 +0100

    Remove tracked node_modules and update gitignore

--- server/models/package.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const PackageSchema = new mongoose.Schema({
-  poids: { type: Number, required: true },
-  type: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Package", PackageSchema);
